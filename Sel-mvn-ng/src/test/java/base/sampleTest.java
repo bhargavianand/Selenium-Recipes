ccb9bb79d2480c179e5765f079c30dc9493fb8a0
vishnu45 <vishnu4v5@gmail.com>
2018-02-09 07:44:09 -0500

    ADD-#2: add more tests

--- Sel-mvn-ng/src/test/java/base/sampleTest.java
+++ Sel-mvn-ng/src/test/java/base/sampleTest.java
@@ -17,7 +17,7 @@ public class sampleTest {
 		driver.get("http://www.google.com");
 		driver.findElement(By.cssSelector("input[id='lst-ib']"))
 			.sendKeys("apple", Keys.ENTER);
-		System.out.println("Test for apple");
+		System.out.println(">>> CONSOLE OUT >>> : Test for apple");
 		driver.quit();
 	}
 	
@@ -29,7 +29,103 @@ public class sampleTest {
 		driver.get("http://www.google.com");
 		driver.findElement(By.cssSelector("input[id='lst-ib']"))
 			.sendKeys("banana", Keys.ENTER);
-		System.out.println("Test for banana");
+		System.out.println(">>> CONSOLE OUT >>> : Test for banana");
+		driver.quit();
+	}
+	
+	@Test(priority = 2, description = "TEST TABLE")
+	public void googleTable() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("table", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for table");
+		driver.quit();
+	}
+	
+	@Test(priority = 1, description = "TEST CHAIR")
+	public void googleChair() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("chair", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for chair");
+		driver.quit();
+	}
+	
+	@Test(priority = 2, description = "TEST RED")
+	public void googleRed() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("red", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for red");
+		driver.quit();
+	}
+	
+	@Test(priority = 1, description = "TEST BLUE")
+	public void googleBlue() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("blue", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for blue");
+		driver.quit();
+	}
+	
+	@Test(priority = 2, description = "TEST GREEN")
+	public void googleGreen() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("green", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for green");
+		driver.quit();
+	}
+	
+	@Test(priority = 1, description = "TEST YELLOW")
+	public void googleYellow() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("yellow", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for yellow");
+		driver.quit();
+	}
+	
+	@Test(priority = 2, description = "TEST TODAY")
+	public void googleToday() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("today", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for today");
+		driver.quit();
+	}
+	
+	@Test(priority = 1, description = "TEST TOMORROW")
+	public void googleTomorrow() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		driver.findElement(By.cssSelector("input[id='lst-ib']"))
+			.sendKeys("tomorrow", Keys.ENTER);
+		System.out.println(">>> CONSOLE OUT >>> : Test for tomorrow");
 		driver.quit();
 	}
 	
